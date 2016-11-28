8a643abd8c5dad9aa5125c57bd60497205741e48
brianqeep <brites009@gmail.com>
2016-11-28 17:40:14 +0300

    functionality added

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
+
 $(document).ready(function(){
   $("#pingpong").submit(function(event){
     event.preventDefault();
     var num = $("#number").val();
     var result = [];
     for (var i = 1; i <= num; i++) {
-      result.push(i);
-      if (i%15==0) {
-        result[i-1]="ping pong";
-      }else if (i%5==0) {
-        result[i-1]="pong";
-      }
-      else if (i%3==0) {
-        result[i-1]="ping";
-      }
+        result.push(i);
+        if (i%15==0) {
+          result[i-1]="pingpong";
+        }else if (i%5==0) {
+          result[i-1]="pong";
+        }
+        else if (i%3==0) {
+          result[i-1]="ping";
+        }
     }
-    var answer = result.join();
-    $("#answer").append(answer);
+    for (var j = 0; j < result.length; j++) {
+        $("#answer").append("<li>"+result[j]+"</li>");
+    }
 
   });
 });
